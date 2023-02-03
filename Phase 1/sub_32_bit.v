bd9d0a3fc6a088e6ffdea7b9a7bfb17c013328d2
Luka Gobovic <19lg26@queensu.ca>
2023-02-03 12:13:26 -0500

    progress

---
+++ Phase 1/sub_32_bit.v
@@ -0,0 +1,8 @@
+module  sub_32_bit (
+    input [31:0] 
+);
+//to subtract, you need to negate the second number, then add it to first
+//To negate a number, you need the two's compliment, which is done by
+//taking the inverse of the number, then adding 1 to it.
+    
+endmodule
